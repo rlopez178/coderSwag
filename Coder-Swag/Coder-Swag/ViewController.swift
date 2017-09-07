5adf078ccbc8c024c75e71771d4cda84ba88b988
rlopez178 <rlopez178@yahoo.com>
2017-09-06 23:16:07 -0400

    Initial Commit

---
+++ Coder-Swag/Coder-Swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by Raymond Lopez on 9/6/17.
+//  Copyright © 2017 OceanBiscuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
